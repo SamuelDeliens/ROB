16f91fc5f9968c98b40e6e1ca90830992d3a94ea
SamuelDeliens <samuel.deliens@gmail.com>
2022-04-21 02:37:44 +0200

    oublie modif ficheir interface

--- code/html/interface/assets/python/script.py
+++ code/html/interface/assets/python/script.py
@@ -1,12 +1,23 @@
+#------------------------------------------------------
+#----------------------- Script -----------------------
+#------------------------------------------------------
+
 import sys
 import socket
 
 sys.path.append("/var/www/html/interface/python")
 print(sys.path)
-# --------------------- SCRIPT -------------------------
 
 from Client import Client
+from BDD import BDD
 
 client = Client()
+
+#----------------------- Config -----------------------
+
 client.configClient('192.168.0.10', 6780)
+client.BDD.configBDD('localhost', 'user', 'pi', 'Rov')
+
+# --------------------- SCRIPT -------------------------
+
 client.execute("GETRT")
